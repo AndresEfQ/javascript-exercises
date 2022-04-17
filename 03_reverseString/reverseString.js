99c0d2b09b5921b0f3ec3665d81af5434ba64e93
AndresEfQ <andresf2641@gmail.com>
2022-04-17 18:46:48 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let array = string.split("");
+  let reverseArray = [];
 
+  for (let i = 0; i < array.length; i++) {
+    reverseArray[i] = array[array.length-1-i];
+  }
+  return reverseArray.join("");
 };
 
 // Do not edit below this line
