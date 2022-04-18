992ec10300d5e75cf5000b5d0fa86a44930b0503
AndresEfQ <andresf2641@gmail.com>
2022-04-17 22:28:18 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+  if (typeof(start) !== "number" || 
+      typeof(end) !== "number" ||
+      start < 0 ||
+      end < 0) {
+    return "ERROR";
+  }
+
+  let result = 0;
+  if ( start <= end) {
+    for (i = start; i <= end; i++) {
+      result += i;
+    }
+  } else {
+    for (i = end; i <= start; i++) {
+      result += i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
