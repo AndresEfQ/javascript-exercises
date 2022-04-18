d5c64af58c3be9df8629ef32455d3f7a7dd4c914
AndresEfQ <andresf2641@gmail.com>
2022-04-17 19:22:56 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+  let result = [];
+  outer: for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < elements.length; j++) {
+      if (array[i] === elements[j]) {
+        continue outer;
+      }
+    }
+    result.push(array[i]);
+  }
+  return result;
 };
 
 // Do not edit below this line
