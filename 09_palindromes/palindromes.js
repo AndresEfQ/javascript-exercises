123f5cb79fce624eccb998af82e669252a808f9a
AndresEfQ <andresf2641@gmail.com>
2022-05-09 02:53:36 -0500

    Update palindromes.js with proposed solution
    The function has been tested manually. TODO: test with jest.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let strippedString = Array.from(string.toLowerCase().split('')).filter((char) => (/[a-z]/).test(char));
+  
+  for (let i = 0; i < strippedString.length / 2; i++ ) {
+    if (strippedString[i] !== strippedString[strippedString.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
