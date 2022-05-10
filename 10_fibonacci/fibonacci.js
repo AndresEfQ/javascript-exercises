fd643ff417cd374d1dc3ed4fb2c72c6b5a54b0fc
AndresEfQ <andresf2641@gmail.com>
2022-05-09 20:17:30 -0500

    Update 10_fibonacci with recursive memo solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n, memo = []) {
+  if (n < 1) return 'OOPS';
+  if (n == 1) return 1;
+  if (n == 2) return 1;
+  if (n in memo) {
+    return memo.n;
+  } else {
+    memo.n = fibonacci(n -1, memo) + fibonacci (n - 2, memo);
+  }
+  return memo.n;
 };
 
 // Do not edit below this line
